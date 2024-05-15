d6d7adbcc18695f374992aac565c4ee16aad060b
Leslie Hoare <iam@lesleh.co.uk>
2024-05-15 01:16:29 +0100

    Force dynamic

--- src/app/components/Slow/Slow.tsx
+++ src/app/components/Slow/Slow.tsx
@@ -1,6 +1,7 @@
+import { clamp } from "@/app/utils/clamp";
 import { delay } from "@/app/utils/delay";
 
-export async function Slow() {
-  await delay(2000);
-  return <p>Slow component</p>;
+export async function Slow({ howSlow = 1000 }: { howSlow?: number }) {
+  await delay(clamp(howSlow, 0, 5000));
+  return <p>Slow component {new Date().toTimeString()}</p>;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,23 @@
-import Image from "next/image";
 import { Slow } from "./components/Slow";
+import { Suspense } from "react";
+
+export const dynamic = "force-dynamic";
 
 export default function Home() {
   return (
-    <main className="container mx-auto p-4">
-      <div className="prose">
-        <Slow />
-        <h1>Welcome to your Next.js app!</h1>
-        <h2>
-          Get started by editing <code>pages/index.js</code>
-        </h2>
-        <p>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </div>
-    </main>
+    <Suspense fallback={<div>Loading...</div>}>
+      <main className="container mx-auto p-4">
+        <div className="prose">
+          <Slow howSlow={300} />
+          <h1>Welcome to your Next.js app!</h1>
+          <h2>
+            Get started by editing <code>pages/index.js</code>
+          </h2>
+          <p>
+            Get started by editing <code>pages/index.js</code>
+          </p>
+        </div>
+      </main>
+    </Suspense>
   );
 }
